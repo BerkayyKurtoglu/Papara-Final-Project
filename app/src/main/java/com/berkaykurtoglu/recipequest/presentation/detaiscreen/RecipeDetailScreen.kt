c4b53db7f20f8403c68360ccb579742347432a63
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-05-31 21:43:32 +0300

    Initial Creation For HomeScreen and RecipeDetailScreen

---
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/detaiscreen/RecipeDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.berkaykurtoglu.recipequest.presentation.detaiscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun RecipeDetailScreen(
+    modifier: Modifier = Modifier,
+    coroutineScope: CoroutineScope
+) {
+
+
+
+}
+
+
---
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/homescreen/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.berkaykurtoglu.recipequest.presentation.homescreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.CoroutineScope
+
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    coroutineScope: CoroutineScope,
+    onNavigateToDetail : () -> Unit
+) {
+
+
+
+}
+
+
