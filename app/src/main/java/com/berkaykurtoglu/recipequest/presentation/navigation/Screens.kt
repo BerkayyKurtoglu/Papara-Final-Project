fe010d5561aa0803ec4131d28ddb8dc3d5222c81
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-01 17:46:15 +0300

    Created Screens.kt for Keeping The Nav Argument's Id and NavTypes as well.

--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/navigation/Screens.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/navigation/Screens.kt
@@ -1,9 +1,21 @@
 package com.berkaykurtoglu.recipequest.presentation.navigation
 
+import androidx.navigation.NavType
+
 sealed class Screens(val route: String) {
 
     data object DetailScreen : Screens("login")
-    data object HomeScreen : Screens("home")
+    data object HomeScreen : Screens("home/")
     data object SplashScreen : Screens("splash")
 
 }
+
+enum class ScreenArguments(
+    val argument: String,
+    val type : NavType<*>
+){
+
+    HOME_SCREEN_ARGUMENT("isNetworkAvailable", NavType.BoolType)
+
+}
+
