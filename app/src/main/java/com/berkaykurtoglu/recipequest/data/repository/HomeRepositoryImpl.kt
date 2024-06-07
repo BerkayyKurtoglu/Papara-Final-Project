96ac2611e661b8bb7910b20b34113a47b6240a90
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-07 21:33:53 +0300

    Implemented HomeRepository and HomeRepositoryImpl

--- app/src/main/java/com/berkaykurtoglu/recipequest/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/data/repository/HomeRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package com.berkaykurtoglu.recipequest.data.repository
 
+import com.berkaykurtoglu.recipequest.data.mapextension.toLocal
+import com.berkaykurtoglu.recipequest.data.source.local.RecipeDao
+import com.berkaykurtoglu.recipequest.data.source.local.entity.LocalRecipeResponse
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.RecipeResponseDto
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
 import com.berkaykurtoglu.recipequest.domain.repository.HomeRepository
@@ -8,11 +11,36 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor (
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: RecipeDao
 ) : HomeRepository {
 
-    override fun getRecipesRandomly() : Flow<ApiResult<RecipeResponseDto>> {
-        return remoteDataSource.getRecipeRandomly()
+    override suspend fun getAllRecipesFromNetwork() : Flow<ApiResult<RecipeResponseDto>> {
+        val response = remoteDataSource.getRecipeRandomly()
+        response.collect{
+            when(it) {
+                is ApiResult.Success -> {
+                    deleteAllDatabase()
+                    updateLocalDatabase(it.data.toLocal())
+                }
+                else ->{}
+            }
+        }
+        return response
     }
+
+    override suspend fun getAllRecipesFromLocal(): Flow<List<LocalRecipeResponse>>
+        =localDataSource.getAllRecipes()
+
+    private suspend fun updateLocalDatabase(
+        localRecipeResponse : List<LocalRecipeResponse>
+    ){
+        localDataSource.insertAllRecipes(localRecipeResponse)
+    }
+
+    private suspend fun deleteAllDatabase(){
+        localDataSource.deleteAllRecipes()
+    }
+
 
 }
--- app/src/main/java/com/berkaykurtoglu/recipequest/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/domain/repository/HomeRepository.kt
@@ -1,13 +1,14 @@
 package com.berkaykurtoglu.recipequest.domain.repository
 
+import com.berkaykurtoglu.recipequest.data.source.local.entity.LocalRecipeResponse
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.RecipeResponseDto
 import com.berkaykurtoglu.recipequest.util.ApiResult
 import kotlinx.coroutines.flow.Flow
 
 interface HomeRepository {
 
+    suspend fun getAllRecipesFromNetwork() : Flow<ApiResult<RecipeResponseDto>>
 
-    fun getRecipesRandomly() : Flow<ApiResult<RecipeResponseDto>>
-
+    suspend fun getAllRecipesFromLocal() : Flow<List<LocalRecipeResponse>>
 
 }
