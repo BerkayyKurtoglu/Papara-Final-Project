bef74d4600819974c3834ce319f2a4ef9770a906
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-07 21:40:08 +0300

    Updated provideHomeRepository DI

--- app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.berkaykurtoglu.recipequest.di
 import android.net.ConnectivityManager
 import com.berkaykurtoglu.recipequest.data.repository.HomeRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.repository.SplashScreenRepositoryImpl
+import com.berkaykurtoglu.recipequest.data.source.local.RecipeDao
 import com.berkaykurtoglu.recipequest.data.source.remote.RecipeApiService
 import com.berkaykurtoglu.recipequest.data.source.remote.RemoteDataSourceImpl
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
@@ -27,8 +28,9 @@ object RepositoryModule{
     @Singleton
     @Provides
     fun provideHomeRepository(
-        remoteDataSource: RemoteDataSource
-    ) : HomeRepository = HomeRepositoryImpl(remoteDataSource)
+        remoteDataSource: RemoteDataSource,
+        localDataSource: RecipeDao
+    ) : HomeRepository = HomeRepositoryImpl(remoteDataSource,localDataSource)
 
     @Singleton
     @Provides
