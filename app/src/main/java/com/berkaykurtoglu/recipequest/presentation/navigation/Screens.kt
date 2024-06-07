283af819889bfeb9d07fa68cde1bbfdf0d59b827
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-07 21:38:49 +0300

    Updated Screens Sealed Class for home screen [route from 'login' to 'home']

--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/navigation/Screens.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/navigation/Screens.kt
@@ -3,7 +3,7 @@ package com.berkaykurtoglu.recipequest.presentation.navigation
 
 sealed class Screens(val route: String) {
 
-    data object DetailScreen : Screens("login")
+    data object DetailScreen : Screens("detail")
     data object HomeScreen : Screens("home")
     data object SplashScreen : Screens("splash")
 
