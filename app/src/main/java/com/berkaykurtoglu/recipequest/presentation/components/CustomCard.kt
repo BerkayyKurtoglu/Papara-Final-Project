9770cad0ef87c890497f70dfe47616de6ba4563c
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-09 16:55:46 +0300

    Resigned HomeScreen Custom Card

--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/components/CustomCard.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/components/CustomCard.kt
@@ -1,6 +1,5 @@
 package com.berkaykurtoglu.recipequest.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,33 +14,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessTime
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material.icons.rounded.FavoriteBorder
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
-import coil.transform.RoundedCornersTransformation
-import com.berkaykurtoglu.recipequest.R
-import com.berkaykurtoglu.recipequest.domain.model.ResultModel
+import com.berkaykurtoglu.recipequest.domain.model.recipesmodel.ResultModel
 
 @Composable
 fun CustomCard(
