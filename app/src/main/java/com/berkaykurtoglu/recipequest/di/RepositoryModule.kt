bcc9b5d93c031e19390c7aa64f8965f463792a3c
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-09 16:50:10 +0300

    Provided DetailRepository

--- app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.berkaykurtoglu.recipequest.di
 
 import android.net.ConnectivityManager
+import com.berkaykurtoglu.recipequest.data.repository.DetailScreenRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.repository.HomeRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.repository.SplashScreenRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.source.local.RecipeDao
@@ -8,6 +9,7 @@ import com.berkaykurtoglu.recipequest.data.source.remote.RecipeApiService
 import com.berkaykurtoglu.recipequest.data.source.remote.RemoteDataSourceImpl
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
 import com.berkaykurtoglu.recipequest.domain.repository.HomeRepository
+import com.berkaykurtoglu.recipequest.domain.repository.DetailScreenRepository
 import com.berkaykurtoglu.recipequest.domain.repository.SplashScreenRepository
 import dagger.Module
 import dagger.Provides
@@ -31,6 +33,12 @@ object RepositoryModule{
         remoteDataSource: RemoteDataSource,
         localDataSource: RecipeDao
     ) : HomeRepository = HomeRepositoryImpl(remoteDataSource,localDataSource)
+
+    @Singleton
+    @Provides
+    fun provideDetailRepository(
+        remoteDataSource: RemoteDataSource
+    ) : DetailScreenRepository = DetailScreenRepositoryImpl(remoteDataSource)
 
     @Singleton
     @Provides
