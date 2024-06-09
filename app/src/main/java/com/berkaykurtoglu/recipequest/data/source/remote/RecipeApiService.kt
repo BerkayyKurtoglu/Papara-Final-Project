729c172f5348598f972fa6a855ba19f2957b9f0f
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-09 16:53:03 +0300

    Implemented getRecipeById in ApiService

--- app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RecipeApiService.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RecipeApiService.kt
@@ -1,8 +1,10 @@
 package com.berkaykurtoglu.recipequest.data.source.remote
 
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.specificdto.RecipeDetailDto
 import com.berkaykurtoglu.recipequest.util.Contants
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RecipeApiService {
@@ -16,5 +18,11 @@ interface RecipeApiService {
         @Query("number") number: Int = 50,
         @Query("sort") sort : String = "random"
     ) : RecipeResponseDto
+
+    @GET(Contants.GET_RECIPE_DETAIL)
+    suspend fun getRecipeById(
+        @Path("id") id: Int,
+        @Query("apiKey") apiKey: String = Contants.API_KEY,
+        ) : RecipeDetailDto
 
 }
