78666c0a4fb4422492dcd45fe0ffb89110610e99
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-09 12:52:36 +0300

    Implemented OnFetchRecipes in the HomeScreenEvent

--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/homescreen/HomeScreenEvent.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/homescreen/HomeScreenEvent.kt
@@ -1,7 +1,10 @@
 package com.berkaykurtoglu.recipequest.presentation.homescreen
 
+import com.berkaykurtoglu.recipequest.util.FilterCategorie
+
 sealed class HomeScreenEvent {
 
-    data class OnFirstCall(val isConnected : Boolean) : HomeScreenEvent()
+    data class OnFetchRecipes(val isConnected : Boolean, val offset : Int) : HomeScreenEvent()
+    data class OnFilter(val filter : FilterCategorie) : HomeScreenEvent()
 
 }
