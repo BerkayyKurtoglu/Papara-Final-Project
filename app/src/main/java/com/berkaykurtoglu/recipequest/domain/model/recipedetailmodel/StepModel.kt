d53aeb7d958991fc4be2c10d4434a1b6ee2b27d8
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-10 15:17:52 +0300

    Making Some Fields Nullable

--- app/src/main/java/com/berkaykurtoglu/recipequest/domain/model/recipedetailmodel/StepModel.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/domain/model/recipedetailmodel/StepModel.kt
@@ -1,10 +1,9 @@
 package com.berkaykurtoglu.recipequest.domain.model.recipedetailmodel
 
-
 import com.google.gson.annotations.SerializedName
 
 data class StepModel(
-    val equipmentModels: List<EquipmentModel>,
+    val equipmentModels: List<EquipmentModel>?,
     val ingredientModels: List<IngredientModel>,
     val number: Int,
     val step: String
