7c302148e05ac6f5c64dc87b9772fee3a43dd57e
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-10 14:12:21 +0300

    Implemented TabRow

---
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/components/CustomTabRow.kt
@@ -0,0 +1,111 @@
+package com.berkaykurtoglu.recipequest.presentation.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.berkaykurtoglu.recipequest.domain.model.recipedetailmodel.AnalyzedInstructionModel
+import com.berkaykurtoglu.recipequest.domain.model.recipedetailmodel.ExtendedIngredientModel
+import com.berkaykurtoglu.recipequest.domain.model.recipedetailmodel.StepModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun CustomTabRow(
+    modifier : Modifier = Modifier,
+    coroutineScope: CoroutineScope,
+    ingredients: List<ExtendedIngredientModel>,
+    instructionSteps: List<StepModel>
+) {
+
+    val tabs = listOf("Ingredients", "Instructions")
+    val pagerState = rememberPagerState {
+        2
+    }
+
+    Column(
+        modifier = modifier
+    ) {
+        PrimaryTabRow(
+            selectedTabIndex = pagerState.currentPage,
+            divider = {}
+        ) {
+            tabs.forEachIndexed{index, title ->
+                Tab(
+                    selected = pagerState.currentPage==index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    modifier = Modifier.padding(vertical = 10.dp),
+                    unselectedContentColor = Color.Gray
+                ) {
+                    Text(
+                        text = tabs[index]
+                    )
+                }
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            contentPadding = PaddingValues(vertical = 15.dp),
+            pageSpacing = 10.dp
+        ) {page->
+            Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.weight(1f)
+            ) {
+                when(page){
+                    0 -> {
+                        ingredients.forEach {
+                            IngredientsView(extendedIngredientModel = it)
+                        }
+                    }
+                    1 -> {
+                        instructionSteps.forEach {step->
+                            Text(
+                                text = buildAnnotatedString {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.Black,
+                                            fontWeight = FontWeight.Bold,
+                                        )
+                                    ){
+                                        append("${step.number} - ")
+                                    }
+                                    append(step.step)
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/components/IngredientsView.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/components/IngredientsView.kt
@@ -1,27 +1,18 @@
 package com.berkaykurtoglu.recipequest.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.berkaykurtoglu.recipequest.R
 import com.berkaykurtoglu.recipequest.domain.model.recipedetailmodel.ExtendedIngredientModel
 
 
 @Composable
-fun DetailItem(
+fun IngredientsView(
     modifier: Modifier = Modifier,
     extendedIngredientModel: ExtendedIngredientModel
 ) {
