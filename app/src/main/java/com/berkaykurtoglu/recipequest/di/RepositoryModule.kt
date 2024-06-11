33173373a74821536195c2a64b9693dae56e9d01
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-11 09:59:06 +0300

    Provided FavoritesRepository

--- app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/di/RepositoryModule.kt
@@ -2,14 +2,17 @@ package com.berkaykurtoglu.recipequest.di
 
 import android.net.ConnectivityManager
 import com.berkaykurtoglu.recipequest.data.repository.DetailScreenRepositoryImpl
+import com.berkaykurtoglu.recipequest.data.repository.FavoriteScreenRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.repository.HomeRepositoryImpl
 import com.berkaykurtoglu.recipequest.data.repository.SplashScreenRepositoryImpl
-import com.berkaykurtoglu.recipequest.data.source.local.RecipeDao
+import com.berkaykurtoglu.recipequest.data.source.local.cachedatabase.CacheDao
+import com.berkaykurtoglu.recipequest.data.source.local.favoritesdatabase.FavoritesDao
 import com.berkaykurtoglu.recipequest.data.source.remote.RecipeApiService
 import com.berkaykurtoglu.recipequest.data.source.remote.RemoteDataSourceImpl
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
 import com.berkaykurtoglu.recipequest.domain.repository.HomeRepository
 import com.berkaykurtoglu.recipequest.domain.repository.DetailScreenRepository
+import com.berkaykurtoglu.recipequest.domain.repository.FavoriteScreenRepository
 import com.berkaykurtoglu.recipequest.domain.repository.SplashScreenRepository
 import dagger.Module
 import dagger.Provides
@@ -31,21 +34,29 @@ object RepositoryModule{
     @Provides
     fun provideHomeRepository(
         remoteDataSource: RemoteDataSource,
-        localDataSource: RecipeDao
+        localDataSource: CacheDao
     ) : HomeRepository = HomeRepositoryImpl(remoteDataSource,localDataSource)
 
     @Singleton
     @Provides
     fun provideDetailRepository(
         remoteDataSource: RemoteDataSource,
-        localDataSource: RecipeDao
-    ) : DetailScreenRepository = DetailScreenRepositoryImpl(remoteDataSource,localDataSource)
+        cacheDataSource: CacheDao,
+        favoritesDataSource : FavoritesDao
+        ) : DetailScreenRepository = DetailScreenRepositoryImpl(remoteDataSource,cacheDataSource,favoritesDataSource)
 
     @Singleton
     @Provides
     fun provideRemoteDataSource(
         apiService: RecipeApiService
     ) : RemoteDataSource = RemoteDataSourceImpl(apiService)
+
+    @Singleton
+    @Provides
+    fun provideFavoritesRepository(
+        favoritesDataSource : FavoritesDao
+    ) : FavoriteScreenRepository = FavoriteScreenRepositoryImpl(favoritesDataSource)
+
 
 }
 
