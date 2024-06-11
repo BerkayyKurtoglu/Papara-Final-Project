5ea4dc28185106a56f86f519b87f8345e2a9d131
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-11 10:33:51 +0300

    Implemented searchRecipes Api Service

--- app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RecipeApiService.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RecipeApiService.kt
@@ -1,17 +1,18 @@
 package com.berkaykurtoglu.recipequest.data.source.remote
 
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.searchdto.RecipeSearchDto
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.specificdto.RecipeDetailDto
-import com.berkaykurtoglu.recipequest.util.Contants
+import com.berkaykurtoglu.recipequest.util.Constants
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RecipeApiService {
 
-    @GET(Contants.COMPLEX_SEARCH)
+    @GET(Constants.COMPLEX_SEARCH)
     suspend fun getRecipes(
-        @Query("apiKey") apiKey: String = Contants.API_KEY,
+        @Query("apiKey") apiKey: String = Constants.API_KEY,
         @Query("addRecipeInformation") addRecipeInformation: Boolean = true,
         @Query("addRecipeNutrition") addRecipeNutrition: Boolean = true,
         @Query("offset") offset: Int = 0,
@@ -19,10 +20,18 @@ interface RecipeApiService {
         @Query("sort") sort : String = "random"
     ) : RecipeResponseDto
 
-    @GET(Contants.GET_RECIPE_DETAIL)
+    @GET(Constants.GET_RECIPE_DETAIL)
     suspend fun getRecipeById(
         @Path("id") id: Int,
-        @Query("apiKey") apiKey: String = Contants.API_KEY,
+        @Query("apiKey") apiKey: String = Constants.API_KEY,
         ) : RecipeDetailDto
+
+    @GET(Constants.AUTO_COMPLETE)
+    suspend fun searchRecipes(
+        @Query("apiKey") apiKey: String = Constants.API_KEY,
+        @Query("query") query: String,
+        @Query("number") number: Int = 10,
+    ) : RecipeSearchDto
+
 
 }
