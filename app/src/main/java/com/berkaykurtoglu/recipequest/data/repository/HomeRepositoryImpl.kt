accb6f61f105221a69281b479eaef40c6e71e37d
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-12 21:57:44 +0300

    Implemented getAllRecipesFromCache, getAllRecipesNetwork, searchRecipesFromNetwork and searchRecipesFromCache in HomeRepository

--- app/src/main/java/com/berkaykurtoglu/recipequest/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/data/repository/HomeRepositoryImpl.kt
@@ -1,45 +1,69 @@
 package com.berkaykurtoglu.recipequest.data.repository
 
-import com.berkaykurtoglu.recipequest.data.mapextension.toLocal
-import com.berkaykurtoglu.recipequest.data.source.local.RecipeDao
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.berkaykurtoglu.recipequest.data.source.local.cachedatabase.CacheDao
 import com.berkaykurtoglu.recipequest.data.source.local.entity.LocalRecipeResponse
+import com.berkaykurtoglu.recipequest.data.source.local.entity.RecipeDetailEntity
+import com.berkaykurtoglu.recipequest.data.source.remote.RecipesPagingSource
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.searchdto.RecipeSearchDto
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
+import com.berkaykurtoglu.recipequest.domain.model.recipesearchmodel.RecipeSearchModel
+import com.berkaykurtoglu.recipequest.domain.model.recipesmodel.RecipeModel
 import com.berkaykurtoglu.recipequest.domain.repository.HomeRepository
 import com.berkaykurtoglu.recipequest.util.ApiResult
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+import com.berkaykurtoglu.recipequest.util.apiFlow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.internal.wait
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor (
     private val remoteDataSource: RemoteDataSource,
-    private val localDataSource: RecipeDao
+    private val cacheDataSource: CacheDao
 ) : HomeRepository {
 
 
     override suspend fun getAllRecipesFromNetwork(
         offset : Int,
         number : Int
-    ) : Flow<ApiResult<RecipeResponseDto>> = remoteDataSource.getRecipeRandomly(offset, number)
+    ) : Flow<ApiResult<RecipeResponseDto>> = remoteDataSource.getRecipesRandomly(offset, number)
 
-    override suspend fun getAllRecipesFromLocal(): Flow<List<LocalRecipeResponse>>
-        =localDataSource.getAllRecipes()
+    override suspend fun getAllRecipesFromCache(): Flow<PagingData<RecipeModel>> =
+        Pager(
+            config = PagingConfig(pageSize = 10, prefetchDistance = 2),
+            pagingSourceFactory = { cacheDataSource.cacheGetAllRecipes() }
+        ).flow.map {pagingData->
+            pagingData.map {
+                it.toRecipeModel()
+            }
+        }
 
-    override suspend fun updateCache(
-        localRecipeResponse : List<LocalRecipeResponse>
-    ) = localDataSource.insertAllRecipes(localRecipeResponse)
+    override suspend fun updateCache(localRecipeResponse: List<LocalRecipeResponse>) {
+        TODO("Not yet implemented")
+    }
 
-    override suspend fun deleteAllCache() : Int=
-        localDataSource.deleteAllRecipes()
+    override suspend fun deleteAllCache(): Int {
+        TODO("Not yet implemented")
+    }
 
+
+    override suspend fun getAllRecipesNetwork(
+        type : String
+    ): Flow<PagingData<RecipeModel>> =
+        Pager(
+            config = PagingConfig(pageSize = 5, prefetchDistance = 0),
+            pagingSourceFactory = { RecipesPagingSource(type,remoteDataSource) }
+        ).flow
+
+    override suspend fun searchRecipesFromNetwork(query: String): Flow<ApiResult<RecipeSearchDto>> =
+        remoteDataSource.searchRecipes(query)
+
+    override suspend fun searchRecipesFromCache(query: String): Flow<ApiResult<List<RecipeDetailEntity>>> =
+        apiFlow {
+            cacheDataSource.cacheSearchRecipes(query)
+        }
 
 }
--- app/src/main/java/com/berkaykurtoglu/recipequest/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/domain/repository/HomeRepository.kt
@@ -1,7 +1,12 @@
 package com.berkaykurtoglu.recipequest.domain.repository
 
+import androidx.paging.PagingData
 import com.berkaykurtoglu.recipequest.data.source.local.entity.LocalRecipeResponse
+import com.berkaykurtoglu.recipequest.data.source.local.entity.RecipeDetailEntity
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.searchdto.RecipeSearchDto
+import com.berkaykurtoglu.recipequest.domain.model.recipesearchmodel.RecipeSearchModel
+import com.berkaykurtoglu.recipequest.domain.model.recipesmodel.RecipeModel
 import com.berkaykurtoglu.recipequest.util.ApiResult
 import kotlinx.coroutines.flow.Flow
 
@@ -12,12 +17,24 @@ interface HomeRepository {
         number : Int
     ) : Flow<ApiResult<RecipeResponseDto>>
 
-    suspend fun getAllRecipesFromLocal() : Flow<List<LocalRecipeResponse>>
+    suspend fun getAllRecipesFromCache() : Flow<PagingData<RecipeModel>>
 
     suspend fun updateCache(
         localRecipeResponse : List<LocalRecipeResponse>
     )
 
     suspend fun deleteAllCache() : Int
+
+    suspend fun getAllRecipesNetwork(
+        type : String
+    ) : Flow<PagingData<RecipeModel>>
+
+    suspend fun searchRecipesFromNetwork(
+        query : String
+    ) : Flow<ApiResult<RecipeSearchDto>>
+
+    suspend fun searchRecipesFromCache(
+        query : String
+    ) : Flow<ApiResult<List<RecipeDetailEntity>>>
 
 }
