2c618f15fad1cf4dd9966bbff234908c5d41b0e1
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-12 22:26:51 +0300

    Implemented getRecipesRandomly, searchRecipes, suggestSimilarRecipe in RemoteDataSource

--- app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/data/source/remote/RemoteDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.berkaykurtoglu.recipequest.data.source.remote
 
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.searchdto.RecipeSearchDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.similarrecipedto.RecipeSimilarDto
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.specificdto.RecipeDetailDto
 import com.berkaykurtoglu.recipequest.domain.datasource.RemoteDataSource
 import com.berkaykurtoglu.recipequest.util.ApiResult
@@ -12,19 +14,28 @@ class RemoteDataSourceImpl @Inject constructor(
     private val apiService: RecipeApiService
 ) : RemoteDataSource {
 
-    override fun getRecipesRandomly(
-        offset : Int,
-        number : Int
-    ): Flow<ApiResult<RecipeResponseDto>> =
-        apiFlow <RecipeResponseDto> {
-            apiService.getRecipes(
-                offset = offset,
-                number = number
-            )
-        }
-
     override fun getRecipeById(id: Int): Flow<ApiResult<RecipeDetailDto>> =
         apiFlow {
             apiService.getRecipeById(id)
         }
+
+    override suspend fun getRecipesRandomly(
+        offset: Int,
+        number: Int,
+        mealType: String
+    ): RecipeResponseDto =
+        apiService.getRecipes(
+            offset = offset,
+            number = number,
+            type = mealType
+        )
+
+    override suspend fun searchRecipes(query: String): Flow<ApiResult<RecipeSearchDto>> =
+        apiFlow {
+            apiService.searchRecipes(query = query)
+        }
+
+    override suspend fun suggestSimilarRecipe(id: Int): RecipeSimilarDto =
+        apiService.getSimilarRecipe(id = id)
+
 }
--- app/src/main/java/com/berkaykurtoglu/recipequest/domain/datasource/RemoteDataSource.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/domain/datasource/RemoteDataSource.kt
@@ -1,19 +1,30 @@
 package com.berkaykurtoglu.recipequest.domain.datasource
 
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.allrecipesdto.RecipeResponseDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.searchdto.RecipeSearchDto
+import com.berkaykurtoglu.recipequest.data.source.remote.dto.similarrecipedto.RecipeSimilarDto
 import com.berkaykurtoglu.recipequest.data.source.remote.dto.specificdto.RecipeDetailDto
 import com.berkaykurtoglu.recipequest.util.ApiResult
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteDataSource {
 
-    fun getRecipesRandomly(
-        offset : Int,
-        number : Int
-    ) : Flow<ApiResult<RecipeResponseDto>>
-
     fun getRecipeById(
         id : Int
     ) : Flow<ApiResult<RecipeDetailDto>>
+
+    suspend fun getRecipesRandomly(
+        offset: Int,
+        number: Int,
+        mealType : String
+    ) : RecipeResponseDto
+
+    suspend fun searchRecipes(
+        query : String
+    ) : Flow<ApiResult<RecipeSearchDto>>
+
+    suspend fun suggestSimilarRecipe(
+        id : Int
+    ) : RecipeSimilarDto
 
 }
