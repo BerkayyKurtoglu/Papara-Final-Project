62a0aa231c6c728e293ab5033df56a05e95a7d38
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-06-12 22:07:07 +0300

    Updated HomeScreenViewModel,getPagingDataFromNetwork,updateChipIndex,getRecipesFromCache

--- app/src/main/java/com/berkaykurtoglu/recipequest/presentation/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/berkaykurtoglu/recipequest/presentation/homescreen/HomeScreenViewModel.kt
@@ -1,86 +1,167 @@
 package com.berkaykurtoglu.recipequest.presentation.homescreen
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.berkaykurtoglu.recipequest.data.mapextension.toModel
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.berkaykurtoglu.recipequest.domain.model.recipesmodel.RecipeModel
+import com.berkaykurtoglu.recipequest.domain.repository.HomeRepository
 import com.berkaykurtoglu.recipequest.domain.usecase.UseCase
 import com.berkaykurtoglu.recipequest.util.ApiResult
 import com.berkaykurtoglu.recipequest.util.FilterCategorie
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@HiltViewModel
+@HiltViewModel()
 class HomeScreenViewModel @Inject constructor(
-    private val useCase : UseCase
+    private val useCase : UseCase,
+    private val homeRepository: HomeRepository,
 ) : ViewModel() {
 
     private val _screenState = MutableStateFlow(HomeScreenState())
     val screenState = _screenState.asStateFlow()
 
+    val screenPagingState : MutableStateFlow<PagingData<RecipeModel>> = MutableStateFlow(PagingData.empty())
+
+    init {
+        fetchRecipes()
+    }
 
     fun onEvent(
         event : HomeScreenEvent
     ){
         when(event){
-            is HomeScreenEvent.OnFirstCall ->{
-                if (event.isConnected) getRecipesFromNetwork()
-                else getRecipesFromDatabase()
+            is HomeScreenEvent.OnFilter ->{
+                updateChipIndex(event.filter)
+                getPagingDataFromNetwork(event.filter)
             }
-        }
-    }
 
-    fun updateChipIndex(filterCategorie: FilterCategorie){
-        _screenState.value = _screenState.value.copy(chipIndex = filterCategorie)
-    }
-
-    private fun getRecipesFromNetwork() {
-        viewModelScope.launch {
-            useCase.getAllRecipesRandomlyUseCase().collect{apiResult ->
-                   when(apiResult) {
-
-                       is ApiResult.Loading ->{
-                           println("loading")
-                       }
-                       is ApiResult.Error ->{
-                           println(apiResult.message)
-                       }
-                       is ApiResult.Success ->{
-                            apiResult.data.results.forEach {
-                                println(it.title)
-                            }
-                       }
-
-                   }
+            is HomeScreenEvent.OnSearchRecipes -> {
+                handleSearchEvent(
+                    query = event.query
+                )
             }
+
+            HomeScreenEvent.RefreshThePage -> fetchRecipes()
         }
     }
 
-    private fun getRecipesFromDatabase(){
-        _screenState.update {
-            it.copy(
-                isLoading = true,
-                errorMessage = ""
-            )
-        }
+    private fun handleSearchEvent(
+        query : String
+    ){
+        if (_screenState.value.isNetworkConnected) searchRecipesFromNetwork(query)
+        else searchRecipesFromCache(query)
+    }
+
+    private fun searchRecipesFromCache(
+        query : String
+    ){
         viewModelScope.launch {
-            delay(1000)
-            useCase.getAllRecipesFromLocalUseCase().collect{localResponseList ->
-                _screenState.update {
-                    it.copy(
-                        isLoading = false,
-                        recipes = localResponseList.toModel(),
-                        errorMessage = ""
-                    )
+            useCase.searchRecipeFromCacheUseCase(query).collect{result->
+                when(result) {
+                    is ApiResult.Error -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = false,
+                            searchErrorMessage = result.message
+                        )
+                    }
+                    ApiResult.Loading -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = true,
+                            searchErrorMessage = ""
+                        )
+                    }
+                    is ApiResult.Success -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = false,
+                            searchRecipesResult = result.data.map { it.toRecipeSearchModel() }
+                        )
+                    }
                 }
             }
         }
     }
 
+    private fun fetchRecipes(){
+        viewModelScope.launch {
+            useCase.checkNetworkUseCase().collect{connected ->
+                _screenState.update {
+                    it.copy(isNetworkConnected = connected)
+                }
+                if(connected) getPagingDataFromNetwork(
+                    _screenState.value.chipIndex
+                )
+                else getRecipesFromCache()
+            }
+        }
+    }
+
+    private fun searchRecipesFromNetwork(
+        query : String
+    ){
+        viewModelScope.launch {
+            useCase.searchRecipesFromNetworkUseCase(query).collect{result->
+                when(result){
+                    is ApiResult.Error -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = false,
+                            searchErrorMessage = result.message
+                        )
+                    }
+                    ApiResult.Loading -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = true,
+                            searchErrorMessage = ""
+                        )
+                    }
+                    is ApiResult.Success -> {
+                        _screenState.value = _screenState.value.copy(
+                            searchIsLoading = false,
+                            searchRecipesResult = result.data.toRecipeSearchModelList()
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getPagingDataFromNetwork(
+        mealType: FilterCategorie
+    ){
+        viewModelScope.launch {
+            homeRepository.getAllRecipesNetwork(
+                when(mealType){
+                    is FilterCategorie.Random -> ""
+                    else -> mealType.category
+                }
+            )
+                .distinctUntilChanged()
+                .cachedIn(viewModelScope)
+                .collect{
+                    screenPagingState.value = it
+                }
+        }
+    }
+
+    private fun updateChipIndex(filterCategorie: FilterCategorie){
+        _screenState.value = _screenState.value.copy(chipIndex = filterCategorie)
+    }
+
+
+    private fun getRecipesFromCache(){
+        viewModelScope.launch {
+            useCase.getAllRecipesFromCache()
+                .distinctUntilChanged()
+                .cachedIn(viewModelScope)
+                .collect{pagingData->
+                    screenPagingState.value = pagingData
+                }
+        }
+    }
 
 }
